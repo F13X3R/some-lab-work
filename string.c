8cbf793bd079fc9825b3ce5d9c2a713d2fca41f7
Egor Byanov <104619895+F13X3R@users.noreply.github.com>
2025-06-06 09:40:41 +0300

    Update string.c

--- string.c
+++ string.c
@@ -1,7 +1,7 @@
-include <stdio.h>
-include <stdlib.h>
-include <string.h>
-include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 define MAX_STRING_LENGTH 1000
 define MAX_SUBSTRINGS 100
